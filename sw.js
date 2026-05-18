66c7625c427a7add8903761d6b2bbe223eab9533
Dynoz769 <adamihaikalgaming@gmail.com>
2026-05-18 15:20:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
-const CACHE_NAME="countdown-v1";
+const CACHE_NAME = "countdown-v2";
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE_NAME).then(cache=>{
-return cache.addAll(["./","./index.html"]);
-})
-);
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./7050b0d9-1c74-436c-b0da-c726e8efca03.jpeg",
+  "./caaf99b3-2a2f-4497-861a-85510cfc104e.jpeg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(res=>res||fetch(e.request))
-);
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
